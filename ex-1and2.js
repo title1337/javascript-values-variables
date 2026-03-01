0b0fefdd26898554af19081768512e1d3a2b6337
Tle <title1337@gmail.com>
2026-03-01 10:04:48 +0700

    feat : add Ex1-2 JavaScirpts

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,26 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let personalName = "Karter";
+console.log(personalName);
+let sirName = "Anderson";
+console.log(sirName);
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+console.log(address);
+
+const restaurantName = "Chef's Table by Chef James";
+console.log(restaurantName);
+const totalPrice = "15000";
+console.log(totalPrice);
+const storeOpen = "1500";
+console.log(storeOpen)
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+console.log(address);
+const totalPrice = "15000";
+totalPrice = "5000";
+console.log(totalPrice);
+const storeOpen = "2500";
+console.log(storeOpen);
