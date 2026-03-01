4a5eefa590359df6d0dc28cc502782754936e97f
Tle <title1337@gmail.com>
2026-03-01 10:16:30 +0700

    feat : Ex3 JavaScirpts

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
-let celsius = 30;
-
 // Start coding here;
+let celsius = 30;
+let fahrenheit = (celsius * 1.8) + 32;
+console.log(fahrenheit);// output: 86
 
-let fahrenheit;
+
